cbd67eba9c4735d77a9d0e7928fb73b8641e550f
Gonzalo Martinez <gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.com>
2023-01-05 02:42:27 -0300

    Se agrego variables de entorno

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import app from "./src/app";
+import { PORT } from "./src/config";
 
-const PORT = process.env.PORT || 3001;
-// const DB_USER = process.env.DB_USER || "localhost";
+
 
 app.listen(PORT, () => {
   console.log(`Corriendo en el puerto ${PORT}`);
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 3001;
+// export const DATABASE_URL = process.env.DATABASE_URL || "localhost";
+export const DB_NAME = process.env.DB_NAME || "fi-back";
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_USER = process.env.DB_USER || "postgres";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "qw12";
+export const DB_PORT = process.env.DB_PORT || "5432";
