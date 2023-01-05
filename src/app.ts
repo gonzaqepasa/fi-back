d58e5d58f01519630dbd12cf0470035a0c5da9f5
Gonzalo Martinez <gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.com>
2023-01-05 03:00:31 -0300

    Probando create en deploy

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import { prisma } from "./db";
 // import routeProducts from "./routes/products";
 // import routeHome from "./routes/home";
 
@@ -14,9 +15,19 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (_req,res) => {
+app.get("/", (_req, res) => {
   res.send("puto eh ");
 });
+
+app.get("/create", async (_req, res) => {
+  const data = await prisma.test.create({
+    data: {
+      name: "Hola",
+    },
+  });
+  res.send(data);
+});
+
 // Rutas
 // app.use('/products',routeProducts)
 // app.use('/home',routeHome)
