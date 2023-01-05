b9505370980b6c5337805c1c5cd588a6329f57bc
Gonzalo Martinez <gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.com>
2023-01-05 02:16:08 -0300

    Correccion de port

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import app from "./src/app";
 
-const PORT:number = 3001
-app.listen(PORT,()=>{
-    console.log(`Corriendo en el puerto ${PORT}`)
-})
+const PORT = process.env.PORT || 3001;
+// const DB_USER = process.env.DB_USER || "localhost";
+
+app.listen(PORT, () => {
+  console.log(`Corriendo en el puerto ${PORT}`);
+});
