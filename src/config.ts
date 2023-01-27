8c224dbe04c540f68dbdab713e7dc1ef67c95ae0
Gonzalo Martinez <gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.com>
2023-01-26 21:29:15 -0300

    se acomodo el puerto

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const PORT = process.env.PORT || 3001;
+export const PORT = process.env.PORT || 3000;
 // export const DATABASE_URL = process.env.DATABASE_URL || "localhost";
 export const DB_NAME = process.env.DB_NAME || "fi-back";
 export const DB_HOST = process.env.DB_HOST || "localhost";
